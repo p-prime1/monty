706a392a4d320b54e6bed7be7eb5ded52c2f9a44
p-prime1 <preciousnwaka95@gmail.com>
2024-03-31 07:56:10 +0100

    Included header files

--- main.h
+++ main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
