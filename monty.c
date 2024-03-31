f1bb63da07e7d048462d20515a0ab6433b2c3b97
p-prime1 <preciousnwaka95@gmail.com>
2024-03-31 08:20:02 +0100

    Added a property to the main func that calls the push pall and pop func

--- monty.c
+++ monty.c
@@ -6,10 +6,12 @@ extern int top;
 int main(int argc, char *argv[])
 {
 	int fd;
+	int choice;
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read;
 
 	fd = open(argv[1], O_RDONLY);
+	choice = 0;
 	if (fd == -1)
 	{
 		perror("Error opening file");
@@ -23,8 +25,13 @@ int main(int argc, char *argv[])
 		close(fd);
 		exit(EXIT_FAILURE);
 	}
-
-
+	strtok(buffer, " ");
+	if (strcmp(buffer, "push") == 0)
+		push(buffer);
+	else if (strcmp(buffer, "pop") == 0)
+		pop();
+	else if (strcmp(buffer, "pall") == 0)
+		pall();
 
 	close(fd);
 }
